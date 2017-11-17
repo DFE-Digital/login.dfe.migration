1ebefd81146e2731b6079e3602d41c970d44343e
Simon Bull <simon.bull.84@googlemail.com>
2017-11-17 08:07:27 +0000

    Add static orgs implementation

--- src/infrastructure/organisations/index.js
+++ src/infrastructure/organisations/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./organisationsApi');
+const config = require('./../config');
+let organisations;
+
+if (config.organisations.type.toLowerCase() === 'static') {
+  organisations = require('./staticOrganisations');
+} else if (config.organisations.type.toLowerCase() === 'api') {
+  organisations = require('./organisationsApi');
+} else {
+  throw new Error(`Unknown organisations type ${config.organisations.type}`);
+}
+
+module.exports = organisations;
