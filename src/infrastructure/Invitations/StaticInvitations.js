c24230879f451705d3953d5a41295a793f0a1441
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-11-17 14:08:46 +0000

    Update static invitation service

--- src/infrastructure/Invitations/StaticInvitations.js
+++ src/infrastructure/Invitations/StaticInvitations.js
@@ -8,9 +8,15 @@ class StaticInvitationsApi extends Invitation {
   }
   static async validateOsaCredentials(id, username, password){
     if(username.toLowerCase() === 'foo@example.com' && password === 'Password1'){
-      return Promise.resolve(true);
+      return Promise.resolve(
+        {
+          id: '12345',
+          firstName: 'Test',
+          lastName: 'Tester',
+          email: 'test@test.com'
+        });
     }
-    return Promise.resolve(false);
+    return Promise.resolve(null);
   }
 }
 
