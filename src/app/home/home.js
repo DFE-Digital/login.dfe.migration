05c52b0cea664b607302a065ee8a51991b7a7eb2
Simon Bull <simon.bull.84@googlemail.com>
2017-11-30 16:07:42 +0000

    Add tests for variable welcome message

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -4,11 +4,15 @@ const invitations = require('./../../infrastructure/Invitations');
 
 const home = async (req, res) => {
   const invitation = await invitations.getById(req.params.id);
+  if (!invitation) {
+    return res.status(404).send();
+  }
+
   let viewName = 'home/views/home';
   if (invitation.oldCredentials.source === 'EAS') {
     viewName = 'home/views/home-eas';
   }
-  res.render(viewName, {
+  return res.render(viewName, {
     title: 'Access DfE services',
     id: req.params.id,
   });
