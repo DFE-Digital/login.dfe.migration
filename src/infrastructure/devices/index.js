969786d3cb06e9c8cd7bdbf42b80474cf47e1c92
Simon Bull <simon.bull.84@googlemail.com>
2017-12-04 11:42:24 +0000

    Use config to select devices adapter

--- src/infrastructure/devices/index.js
+++ src/infrastructure/devices/index.js
@@ -1,1 +1,10 @@
-module.exports = require('./devicesApi');
+/* eslint-disable global-require */
+const config = require('./../config');
+
+let adapter;
+if (config.devices.type === 'static') {
+  adapter = require('./static');
+} else if (config.devices.type === 'api') {
+  adapter = require('./devicesApi');
+}
+module.exports = adapter;
