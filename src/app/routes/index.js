d4efd78e9f19ce220ec78dd9023558b065fc2467
Simon Bull <simon.bull.84@googlemail.com>
2018-10-17 16:00:37 +0100

    Redirect invite link to profiles and remove all other routes

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,14 +2,14 @@
 
 
 const devRoutes = require('./../devLauncher');
-const migrationHome = require('./../home');
-const osaAuth = require('../osaAuth');
-const easAuth = require('../easAuth');
-const enterCode = require('../enterCode');
-const easToken = require('../easToken');
-const userDetails = require('./../userDetails');
-const newPassword = require('./../newPassword');
-const complete = require('./../complete');
+// const migrationHome = require('./../home');
+// const osaAuth = require('../osaAuth');
+// const easAuth = require('../easAuth');
+// const enterCode = require('../enterCode');
+// const easToken = require('../easToken');
+// const userDetails = require('./../userDetails');
+// const newPassword = require('./../newPassword');
+// const complete = require('./../complete');
 const config = require('./../../infrastructure/config');
 const healthCheck = require('login.dfe.healthcheck');
 
@@ -18,14 +18,18 @@ const routes = (app, csrf) => {
   if (config.hostingEnvironment.useDevViews === true) {
     app.use('/dev', devRoutes(csrf));
   }
-  app.use('/osa-auth', osaAuth(csrf));
-  app.use('/eas-auth', easAuth(csrf));
-  app.use('/enter-code', enterCode(csrf));
-  app.use('/eas-token', easToken(csrf));
-  app.use('/my-details', userDetails());
-  app.use('/new-password', newPassword(csrf));
-  app.use('/complete', complete());
-  app.use('/', migrationHome(csrf));
+  // app.use('/osa-auth', osaAuth(csrf));
+  // app.use('/eas-auth', easAuth(csrf));
+  // app.use('/enter-code', enterCode(csrf));
+  // app.use('/eas-token', easToken(csrf));
+  // app.use('/my-details', userDetails());
+  // app.use('/new-password', newPassword(csrf));
+  // app.use('/complete', complete());
+  // app.use('/', migrationHome(csrf));
+
+  app.get('/:id', (req, res) => {
+    res.redirect(`${config.hostingEnvironment.profilesUrl}/register/${req.params.id}`);
+  });
 };
 
 module.exports = routes;
